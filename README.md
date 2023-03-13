e32b2a7a7974435e6b29dbe0542831ec82b2f047
Finn Ellis <finn@finn.fun>
2023-03-12 20:12:50 -0700

    fix content type

---
+++ README.md
@@ -0,0 +1,10 @@
+# Whose Turn Is It?
+
+This is a tiny React app that lets you input a set of names and whose turn it is now, then gives you a URL to return to and see whose turn it is next time. It comes with infrastructure-as-code to deploy it as a static site in S3.
+
+* `npm install`
+* `npm start` to run the local dev server
+* Edit the code in `src/index.js` and the template in `dist/index.html`
+* Follow the instructions in [infrastructure](infrastructure) to enable:
+  * `npm run deploy` to upload the app to S3
+  * `npm run teardown` to remove the S3 deployment
--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "4.58.0"
     }
   }
@@ -10,31 +10,37 @@ terraform {
 provider "aws" {}
 
 variable "bucket_name" {
-    type = string
-    description = "Name of the S3 bucket to create or update."
+  type        = string
+  description = "Name of the S3 bucket to create or update."
 }
 
 resource "aws_s3_bucket" "this" {
-    bucket = var.bucket_name
+  bucket = var.bucket_name
 }
 
 resource "aws_s3_bucket_website_configuration" "this" {
-    bucket = aws_s3_bucket.this.id
-    index_document {
-        suffix = "index.html"
-    }
+  bucket = aws_s3_bucket.this.id
+  index_document {
+    suffix = "index.html"
+  }
 }
 
 locals {
-    dist_dir = "${path.root}/../dist/"
-    files = ["index.html", "bundle.js", "bundle.js.LICENSE.txt"]
+  dist_dir = "${path.root}/../dist/"
+  files = {
+    "index.html" : "text/html",
+    "bundle.js" : "text/javascript",
+    "bundle.js.LICENSE.txt" : "text/plain"
+  }
 }
 
 resource "aws_s3_object" "this" {
-    for_each = toset(local.files)
-    bucket = aws_s3_bucket.this.id
-    key = each.value
-    source = "${local.dist_dir}${each.value}"
+  for_each     = local.files
+  bucket       = aws_s3_bucket.this.id
+  key          = each.key
+  content_type = each.value
+  acl          = "public-read"
+  source       = "${local.dist_dir}${each.key}"
 }
 
 output "url" {
